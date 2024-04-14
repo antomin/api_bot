97b686710e5255529dc5d84535cc707093d2296d
michael <kurashevmichael@gmail.com>
2024-04-14 16:40:39 +0300

    update feature admin page v1

--- flask_app/extensions.py
+++ flask_app/extensions.py
@@ -1,15 +1,31 @@
-from flask_admin import Admin
+from flask_admin import Admin, helpers
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_app import MyAdminIndexView
+# from flask_security import Security
+# from flask_app.adminlte.admin import AdminLte, admins_store
+# from flask import url_for
 
 from common.models import Base
 from common.settings import settings
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate(compare_type=True)
-admin = Admin(name=settings.APP_NAME, index_view=MyAdminIndexView(), template_mode="bootstrap4", base_template='master-extended.html')
 login_manager = LoginManager()
+# security = Security(datastore=admins_store)
+admin = Admin(name=settings.APP_NAME, index_view=MyAdminIndexView(), template_mode="bootstrap4", base_template='master-extended.html')
+# admin = AdminLte(skin='green', name=settings.APP_NAME, short_name="<b>F</b>C", long_name="<b>Flask</b>CMS", base_template='master-extended.html', index_view=MyAdminIndexView())
+
+
+# @security.context_processor
+# def security_context_processor():
+#     return dict(
+#         admin_base_template=admin.base_template,
+#         admin_view=admin.index_view,
+#         h=helpers,
+#         get_url=url_for
+#     )
+
 
 __all__ = ["db", "migrate", "login_manager", "admin"]
