cfb8913833a0e6d822cfee7a2719fb876bfdf284
Anton Mindlin <anton.mindlin@gmail.com>
2024-04-20 00:17:52 +0200

    fixes

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from flask_app.app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="web", port=5000, debug=settings.DEBUG)
+    app.run(host="127.0.0.1", port=5000, debug=settings.DEBUG)
