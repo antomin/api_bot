ea80bbd90f02b931b2e964a9bc850dda16e6e383
Anton Mindlin <anton.mindlin@gmail.com>
2024-05-15 14:41:25 +0200

    Fix complete view

--- migrator.py
+++ migrator.py
@@ -72,43 +72,41 @@ def create_users(users: list[dict], invoices: list[dict]) -> None:
     logger.info("Creating users")
     cnt = len(users)
 
-    with db.session_factory() as session:
-        for user in users:
-            if user["full_name"] == "need_update":
-                continue
+    for user in users:
+        if user["full_name"] == "need_update":
+            continue
 
-            try:
-                names = parse_name(user["full_name"])
-                tariff = calc_tariff(type_=user["type"], quantity=user["quantity"])
+        names = parse_name(user["full_name"])
+        tariff = calc_tariff(type_=user["type"], quantity=user["quantity"])
 
-                user_obj = User(
-                    id=user["user_id"],
-                    username=user["username"],
-                    first_name=names[0],
-                    last_name=names[1],
-                    is_active=True if user["active"] else False,
-                    chatgpt_daily_limit=tariff["chatgpt_daily_limit"],
-                    gemini_daily_limit=tariff["gemini_daily_limit"],
-                    kandinsky_daily_limit=tariff["kandinsky_daily_limit"],
-                    sd_daily_limit=tariff["sd_daily_limit"],
-                    token_balance=int(user["left"]) * 6 if user["left"] else 0,
-                    update_daily_limits_time=calc_update_time(user["created_at"]) if user["created_at"] else datetime.now(),
-                    tariff_id=tariff["tariff_id"],
-                    mother_invoice_id=get_mother_inv_id(user["id"], invoices),
-                    payment_time=str_to_datetime(user["expiration_date"]) if tariff["tariff_id"] else None,
-                    created_at=str_to_datetime(user["created_at"]) if user["created_at"] else datetime.now(),
-                )
+        user_obj = User(
+            id=user["user_id"],
+            username=user["username"],
+            first_name=names[0],
+            last_name=names[1],
+            is_active=True if user["active"] else False,
+            chatgpt_daily_limit=tariff["chatgpt_daily_limit"],
+            gemini_daily_limit=tariff["gemini_daily_limit"],
+            kandinsky_daily_limit=tariff["kandinsky_daily_limit"],
+            sd_daily_limit=tariff["sd_daily_limit"],
+            token_balance=int(user["left"]) * 15 if user["left"] else 0,
+            update_daily_limits_time=calc_update_time(user["created_at"]) if user["created_at"] else datetime.now(),
+            tariff_id=tariff["tariff_id"],
+            mother_invoice_id=get_mother_inv_id(user["id"], invoices),
+            payment_time=str_to_datetime(user["expiration_date"]) if tariff["tariff_id"] else None,
+            created_at=str_to_datetime(user["created_at"]) if user["created_at"] else datetime.now(),
+        )
 
+        try:
+            with db.session_factory() as session:
                 session.add(user_obj)
                 session.commit()
-            except Exception as error:
-                logger.error(f"Creating user <{user['user_id']}> failed: {error}")
-                errors.append({"user_id": user["user_id"], "error": error})
+        except Exception as error:
+            logger.error(f"Creating user <{user['user_id']}> failed: {error.args}")
+            errors.append({"user_id": user["user_id"], "error": error.args})
 
-            cnt -= 1
-            print(f"USERS: {cnt}")
-
-        # session.commit()
+        cnt -= 1
+        print(f"USERS: {cnt}")
 
     logger.info("Creating users FINISH")
 
@@ -118,27 +116,27 @@ def create_links(links: list[dict], users: list[dict], invoices: list[dict]) -> 
     default_owner_id = 5367092501
     cnt = len(links)
 
-    with db.session_factory() as session:
-        for link in links:
-            try:
-                link_obj = ReferalLink(
-                    id=link["id"],
-                    name=link["name"],
-                    owner_id=default_owner_id,
-                    site_link=f"https://chatgpt-neyroset.ru/redirect/{link['id']}/",
-                    bot_link=f"https://t.me/chatgpt_gpt4bot?start={link['id']}",
-                    created_at=str_to_datetime(link["created_at"]),
-                )
+    for link in links:
+        link_obj = ReferalLink(
+            id=link["id"],
+            name=link["name"],
+            owner_id=default_owner_id,
+            site_link=f"https://chatgpt-neyroset.ru/redirect/{link['id']}/",
+            bot_link=f"https://t.me/chatgpt_gpt4bot?start={link['id']}",
+            created_at=str_to_datetime(link["created_at"]),
+        )
 
-                users_ids = [user["user_id"] for user in users if user["link_id"] == link["id"]]
-                invoices_amounts = [int(inv["amount"]) for inv in invoices if inv['payment_status'] == "success" and inv["user_id"] in users_ids]
-                users_cnt = len(users_ids)
+        users_ids = [user["user_id"] for user in users if user["link_id"] == link["id"] and user["full_name"] != "need_update"]
+        invoices_amounts = [int(inv["amount"]) for inv in invoices if inv['payment_status'] == "success" and inv["user_id"] in users_ids]
+        users_cnt = len(users_ids)
 
-                link_obj.new_users = users_cnt
-                link_obj.clicks = users_cnt
-                link_obj.buys_cnt = len(invoices_amounts)
-                link_obj.buys_sum = sum(invoices_amounts)
+        link_obj.new_users = users_cnt
+        link_obj.clicks = users_cnt
+        link_obj.buys_cnt = len(invoices_amounts)
+        link_obj.buys_sum = sum(invoices_amounts)
 
+        try:
+            with db.session_factory() as session:
                 for user_id in users_ids:
                     user = session.get(User, user_id)
                     link_obj.users.append(user)
@@ -148,14 +146,13 @@ def create_links(links: list[dict], users: list[dict], invoices: list[dict]) -> 
                 session.add(link)
                 session.commit()
 
-            except Exception as error:
-                logger.error(f"Creating link <{link['id']}> failed: {error}")
-                errors.append({"link_id": link["id"], "error": error})
+        except Exception as error:
+            logger.error(f"Creating link <{link['id']}> failed: {error.args}")
+            errors.append({"link_id": link["id"], "error": error.args})
 
-            cnt -= 1
-            print(f"LINKS: {cnt}")
+        cnt -= 1
+        print(f"LINKS: {cnt}")
 
-        # session.commit()
         logger.info("Creating links FINISH")
 
 
@@ -170,9 +167,9 @@ def delete_data() -> None:
 
 
 def main():
-    users = get_data("fixtures/json_users.json")
-    invoices = get_data("fixtures/json_invoices.json")
-    links = get_data("fixtures/json_links.json")
+    users = get_data("data/fixtures/json_users.json")
+    invoices = get_data("data/fixtures/json_invoices.json")
+    links = get_data("data/fixtures/json_links.json")
 
     create_users(users, invoices)
     create_links(links, users, invoices)
