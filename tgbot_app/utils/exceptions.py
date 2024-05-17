f6d8428affbef0e815b84f60790b3a0b2fccacc3
Anton Mindlin <anton.mindlin@gmail.com>
2024-05-17 13:10:29 +0200

    Fix recurents

--- tgbot_app/utils/exceptions.py
+++
@@ -1,9 +0,0 @@
-from aiogram.dispatcher.event.bases import CancelHandler
-
-
-class SilentCancelHandler(CancelHandler):
-    def __str__(self):
-        return
-
-    def with_traceback(self, __tb):
-        return
